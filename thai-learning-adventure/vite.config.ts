a56463308998e9bffe628b3b3a1d03e60d5f36ab
Boonyarit Pornaul <144136018+boonyaritpornuan@users.noreply.github.com>
2025-06-10 15:38:02 +0700

    Add files via upload

--- thai-learning-adventure/vite.config.ts
+++ thai-learning-adventure/vite.config.ts
@@ -1,18 +1,17 @@
-// vite.config.js
-export default {
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id
-              .toString()
-              .split('node_modules/')[1]
-              .split('/')[0]
-              .toString();
-          }
-        },
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
-    },
-  },
-};
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
+});
